9e7e3da8c5e2d2d1c70cc15212664fe8359f2573
eh394 <hazla.ewa@gmail.com>
2022-12-12 14:57:18 +0000

    Add milestone 2 and 3.

---
+++ hangman/milestone_2.py
@@ -0,0 +1,24 @@
+import random
+import string
+
+# create a list of letters 
+alphabet = string.ascii_lowercase
+# print(alphabet)
+
+# create a list of words
+word_list = ['raspberry', 'peach', 'blackberry', 'grape', 'plum']
+print(word_list)
+
+# select a random word from the list
+word = random.choice(word_list)
+print(word)
+
+# get user input and verify it
+print('Please enter a single letter: ')
+guess = input()
+
+if len(guess) == 1 and guess in alphabet:
+    print(guess)
+    print('Good guess!')
+else:
+    print('Oops! That is not a valid input.')
---
+++ hangman/milestone_3.py
@@ -0,0 +1,38 @@
+import random
+import string
+
+# create a list of letters 
+alphabet = string.ascii_lowercase
+# print(alphabet)
+
+# create a list of words
+word_list = ['raspberry', 'peach', 'blackberry', 'grape', 'plum']
+print(word_list)
+
+# select a random word from the list
+word = random.choice(word_list)
+print(word)
+
+# get user input and verify it
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f'Good guess! {guess} is in the word.')
+    else:
+        print(f'Sorry, {guess} is not in the word. Try again.')
+
+def ask_for_input():
+    print('Please enter a single letter: ')
+    guess = input()
+    while len(guess) != 1 or guess.lower() not in alphabet:
+        print('Invalid letter. Please enter a single alphabetical character.')
+        guess = input()
+    guess = guess
+    check_guess(guess)
+
+ask_for_input()
+
+
+
+
+
