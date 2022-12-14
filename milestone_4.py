646a432aee14d4bd5e568bd739b2023a710af72e
eh394 <hazla.ewa@gmail.com>
2022-12-14 21:32:11 +0000

    corrected typo

--- milestone_4.py
+++ milestone_4.py
@@ -6,7 +6,7 @@ class Hangman:
     def __init__(self, word_list, num_lives = 5):
         self.word = random.choice(word_list)
         self.word_guessed = ['_'] * len(self.word)
-        self.num_letters = len(set(self.word_guessed))
+        self.num_letters = len(set(self.word))
         self.num_lives = num_lives
         self.word_list = word_list
         self.list_of_guesses = []
