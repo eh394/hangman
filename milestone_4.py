f50812a4f3c2fd8c9e14f151f4beebdb54bff930
eh394 <hazla.ewa@gmail.com>
2022-12-15 20:36:42 +0000

    Redundant self.list_of_guesses.append(guess) line commented out.

--- milestone_4.py
+++ milestone_4.py
@@ -36,7 +36,7 @@ class Hangman:
                 print('You already tried that letter!')
             else:
                 self.check_guess(guess)
-                self.list_of_guesses.append(guess)
+                # self.list_of_guesses.append(guess)
                 break
 
 bla = Hangman(word_list)
